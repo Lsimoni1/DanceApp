3ab1d45608f57f3d6e708b7b2688e0c80f15fb45
Lsimoni1 <luke.simoni2001@gmail.com>
2025-10-25 10:10:03 -0700

    Updated dancer component to include arrow

--- src/components/Dancer.tsx
+++ src/components/Dancer.tsx
@@ -1,21 +1,29 @@
-import { Circle } from "react-konva";
+import { Circle, Group, Arrow, } from "react-konva";
 
 interface DancerProps {
     x: number;
     y: number; 
     color?: string;
     name?: string;
-    isPreview?: boolean; //true when following cursor before placement
+    isPreview?: boolean; 
 }
 
 const Dancer = ({x, y, color = "red", isPreview = false}: DancerProps) => {
     return (
-        <Circle
-            x={x}
-            y={y}
-            radius={12.5}
-            fill={isPreview ? "rgba(255, 0 , 0, 0.5)" : color} //transparent if preview
-        />
+        <Group x={x} y={y} rotation={180}>
+            <Circle
+                radius={12.5}
+                fill={isPreview ? "rgba(255, 0, 0, 0.5)" : color}
+            />
+            <Arrow
+                points={[0, 0, 0, -20]} 
+                pointerLength={5}
+                pointerWidth={9}
+                fill={isPreview ? "rgba(0, 0, 0, 0.5)" : color}
+                stroke={isPreview ? "rgba(0, 0, 0, 0.5)" : color}
+                strokeWidth={2}
+            />
+        </Group>
     );
 };
 
